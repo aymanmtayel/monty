0fc57f553b171a4503b62919da2835d14c422392
Ayman Tayel <eng.aymantayel@gmail.com>
2024-01-19 14:07:19 +0200

    Task00 fix errors

--- _errors.c
+++ _errors.c
@@ -13,24 +13,25 @@ void _error(int error_n, ...)
 
 	va_start(list, error_n);
 
-	line = va_arg(list, int);
-	oper = va_arg(list, char *);
-
 	switch (error_n)
 	{
 		case 1:
 			fprintf(stderr, "USAGE: monty file\n");
 			break;
 		case 2:
+			oper = va_arg(list, char *);
 			fprintf(stderr, "Error: Can't open file %s\n", oper);
 			break;
 		case 3:
+			line = va_arg(list, int);
+			oper = va_arg(list, char *);
 			fprintf(stderr, "L%d: unknown instruction %s\n", line, oper);
 			break;
 		case 4:
 			fprintf(stderr, "Error: malloc failed\n");
 			break;
 		case 5:
+			line = va_arg(list, int);
 			fprintf(stderr, "L%d: usage: push integer\n", line);
 			break;
 		default:
